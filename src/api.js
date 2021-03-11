9abab1c55521655016e6427bf544101cd198141c
Michal Miškerník <michal@miskernik.sk>
2021-03-11 14:45:04 +0100

    Load API URL from environment variable

--- src/api.js
+++ src/api.js
@@ -1,43 +1,43 @@
-const apiUrl = "http://localhost:8080";
+const apiUrl = process.env.REACT_APP_API_URL ?? "http://localhost:8080";
 
 export async function getAuthUrl(redirectUrl) {
   const response = await fetch(apiUrl + "/auth/facebook", {
     method: "POST",
     headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
     },
     body: JSON.stringify({
-      redirect_url: redirectUrl
-    })
-  })
+      redirect_url: redirectUrl,
+    }),
+  });
 
   if (!response.ok) {
-    throw new Error("Get auth URL request failed")
+    throw new Error("Get auth URL request failed");
   }
 
   const data = await response.json();
 
   return {
     authUrl: data.authorization_url,
-    state: data.state
-  }
+    state: data.state,
+  };
 }
 
 export async function finishAuth(redirectUrl, state, params) {
   const response = await fetch(apiUrl + "/auth/facebook/finish", {
     method: "POST",
     headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
     },
     body: JSON.stringify({
       redirect_url: redirectUrl,
       state: state,
       params: params,
-    })
-  })
+    }),
+  });
 
   if (!response.ok) {
-    throw new Error("Finish auth request failed")
+    throw new Error("Finish auth request failed");
   }
 
   const data = await response.json();
@@ -46,10 +46,10 @@ export async function finishAuth(redirectUrl, state, params) {
     return {
       error: data.error,
       retryUrl: data.retry_url,
-    }
+    };
   }
 
   return {
-    email: data.email
-  }
+    email: data.email,
+  };
 }
