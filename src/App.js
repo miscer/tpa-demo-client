ee53cc4e1251f61d0bc2d113974f4df44579a123
Michal Miškerník <michal@miskernik.sk>
2021-03-16 12:46:00 +0100

    Added comments

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { useEffect, useMemo, useState } from "react";
 import { finishAuth, getAuthUrl } from "./api";
 
 function App() {
+  // Result of the authentication process - either an error object, or a success object with an email address
   const [result, setResult] = useState(null);
   const logOut = () => setResult(null);
 
   return (
     <Router>
       <Switch>
+        {/* This is where the third party will redirect the user after logging in */}
         <Route
           path="/auth/:provider"
           render={(props) => <AuthCallback {...props} onFinish={setResult} />}
         />
+
         <Route
           path="/"
           render={(props) => (
@@ -24,6 +27,7 @@ function App() {
   );
 }
 
+// URL where the third party should redirect the user after loggin in
 const redirectUrl = window.location.origin + "/auth/facebook";
 
 function Content(props) {
@@ -39,6 +43,8 @@ function Content(props) {
 function LoginForm() {
   const logInWithFacebook = async () => {
     const { authUrl, state } = await getAuthUrl(redirectUrl);
+    // State needs to be saved so we can send it to backend later. In the real app other things would be saved as well,
+    // for example the job opening slug, T&C version, page etc.
     localStorage.setItem("state", state);
     window.location.assign(authUrl);
   };
@@ -82,7 +88,10 @@ function Result(props) {
 function AuthCallback(props) {
   const { onFinish, location, history } = props;
 
+  // State should have been saved when the user started logging in
   const state = localStorage.getItem("state");
+
+  // Parse query parameters into a plain object
   const params = useMemo(() => {
     const search = new URLSearchParams(location.search);
     return Object.fromEntries(search.entries());
@@ -92,6 +101,7 @@ function AuthCallback(props) {
     const result = await finishAuth(redirectUrl, state, params);
     onFinish(result);
 
+    // Remove the current URL with authorization code from browser history and go back to app content
     history.replace("/");
   };
 
@@ -99,6 +109,7 @@ function AuthCallback(props) {
     handleCallback(state, params);
   }, [state, params]);
 
+  // Show a loader while we are waiting for backend to fetch user info from the third party
   return (
     <div>
       <p>Loading...</p>
